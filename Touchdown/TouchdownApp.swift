b6fd6f65e8ea34bd8b09334eb9e03ea112c40a04
Amin-0-1 <mahmoudamin373@gmail.com>
2023-12-13 17:51:56 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Amin on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
